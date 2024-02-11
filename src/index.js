db81a563b8631075b09dba3bd08d81e9212d964d
Sabith T H <sabith.th.07@gmail.com>
2024-02-11 19:46:50 +0100

    Enable introspection and Apollo Sandbox

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import 'dotenv/config';
 import TvdbAPI from './datasources/TvdbAPI';
 import resolvers from './resolvers/index';
@@ -9,7 +10,12 @@ import tokenRefresher, { setNewToken } from './tokenRefresher';
 setNewToken();
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  });
   const { url } = await startStandaloneServer(server, {
     context: ({ req }) => {
       const token = req.headers.authorization || process.env.TOKEN || '';
